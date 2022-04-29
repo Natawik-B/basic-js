2a2f5bebffd54308d89b5596d2e2fc31f187b7d7
Natawik-B <natawwwik37@gmail.com>
2022-04-29 17:37:58 +0300

    feat: add the solution of 19 task (passing: 7 and failing: 1)

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-}
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  if (arr.length == 0) {
+    return [];
+  }
+
+  let array = [...arr];
+  let moves = ['--double-prev', '--discard-prev', '--discard-next', '--double-next']
+  for (i = 0; i < moves.length; i++) {
+    let item = array.indexOf(moves[i]);
+    if (item >= 0) {
+      switch(i) {
+        case 2:
+          array.splice(item, 2, '');
+        break;
+
+        case 1:
+          if (item == 0) {
+            array.splice(item, 1, '');
+          } else {
+            array.splice(item - 1, 2, '');
+          }
+        break;
+        
+        case 3:
+          if (item == (array.length - 1)) {
+            array.splice(item, 1, '');
+          } else {
+            array.splice(item, 1, array[item + 1]);
+          }
+        break;
+
+        case 0:
+          if (item == 0) {
+          array.splice(item, 1, '')
+          } else {
+            array.splice(item, 1, array[item - 1]);
+          }
+        break;
+        default: alert('error');
+      }
+    }
+  }
+  let result = array.filter(e => (e != ''));
+  return result;
+};
 
 module.exports = {
   transform
